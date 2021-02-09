ff9da275c3c72e1cfaaab664dee225b80f201a22
esgalimov <esgalimov@gmail.com>
2021-02-09 18:26:04 +0300

    Задача 2. Подтормаживает, требует доработки

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from map import map
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
 
 SCREEN_SIZE = [600, 450]
 FILE_NAME = 'map.png'
@@ -20,16 +21,31 @@ class Example(QWidget):
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
-
-        ## Изображение
-        self.pixmap = QPixmap(FILE_NAME)
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
+        self.new_map()
+
+    def new_map(self):
+        self.pixmap = QPixmap(FILE_NAME)
         self.image.setPixmap(self.pixmap)
+        self.image.update()
 
     def closeEvent(self, event):
         os.remove(FILE_NAME)
+
+    def keyPressEvent(self, event):
+        global SPN
+        if event.key() == Qt.Key_PageUp:
+            if SPN >= 0:
+                SPN -= SPN / 5
+            map(LONGITUDE, LATTITUDE, SPN)
+            self.new_map()
+        if event.key() == Qt.Key_PageDown:
+            if SPN >= 0:
+                SPN += SPN / 5
+            map(LONGITUDE, LATTITUDE, SPN)
+            self.new_map()
 
 
 if __name__ == '__main__':
