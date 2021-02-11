f59b6f743d24242cf114a8f9ea32ce7ba6d174c8
esgalimov <esgalimov@gmail.com>
2021-02-11 23:25:05 +0300

    переключение слоев

--- main.py
+++ main.py
@@ -4,42 +4,64 @@ from map import map
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtCore import Qt
-from qt.qt_for_tasks_1_3 import *
+from qt.qt import *
 
 FILE_NAME = 'map.png'
 LONGITUDE = 33
 LATTITUDE = 55
 SPN = 25
 MOVE = 10
+TYPE_OF_MAP = 'map'
+NAMES = ['map.png', 'map.jpg']
 
 
 class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        map(LONGITUDE, LATTITUDE, SPN)
+        map(LONGITUDE, LATTITUDE, SPN, TYPE_OF_MAP, FILE_NAME)
         self.initUI()
 
     def initUI(self):
+        self.pushButton_gibrid.clicked.connect(self.change_type)
+        self.pushButton_sputnik.clicked.connect(self.change_type)
+        self.pushButton_sxema.clicked.connect(self.change_type)
+
         self.pixmap = QPixmap(FILE_NAME)
         self.new_map()
 
+    def change_type(self):
+        global TYPE_OF_MAP, FILE_NAME
+        if self.sender() == self.pushButton_gibrid:
+            TYPE_OF_MAP = 'sat,skl'
+            FILE_NAME = NAMES[1]
+        elif self.sender() == self.pushButton_sputnik:
+            TYPE_OF_MAP = 'sat'
+            FILE_NAME = NAMES[1]
+        elif self.sender() == self.pushButton_sxema:
+            TYPE_OF_MAP = 'map'
+            FILE_NAME = NAMES[0]
+        self.new_map()
+
     def new_map(self):
+        map(LONGITUDE, LATTITUDE, SPN, TYPE_OF_MAP, FILE_NAME)
         self.pixmap = QPixmap(FILE_NAME)
         self.map.setPixmap(self.pixmap)
         self.map.update()
 
     def closeEvent(self, event):
-        os.remove(FILE_NAME)
+        for i in NAMES:
+            if os.path.exists(i):
+                os.remove(i)
 
     def keyPressEvent(self, event):
         global SPN, LONGITUDE, LATTITUDE
         if event.key() == Qt.Key_PageUp:
             if SPN >= 0:
-                SPN -= SPN
+                SPN = round(SPN * 0.5, 6)
         if event.key() == Qt.Key_PageDown:
-            if SPN < 50:
-                SPN += SPN
+            if SPN < 26:
+                SPN = round(SPN * 1.5, 6)
         if event.key() == Qt.Key_Left:
             if LONGITUDE > -170:
                 LONGITUDE -= MOVE
@@ -52,8 +74,8 @@ class Example(QMainWindow, Ui_MainWindow):
         if event.key() == Qt.Key_Down:
             if LATTITUDE > -80:
                 LATTITUDE -= MOVE
-        map(LONGITUDE, LATTITUDE, SPN)
         self.new_map()
+        print(LONGITUDE, LATTITUDE, SPN, TYPE_OF_MAP)
 
 
 if __name__ == '__main__':
--- map.py
+++ map.py
@@ -2,12 +2,12 @@ import requests
 import sys
 
 
-def map(coord_1, coord_2, spn):
+def map(coord_1, coord_2, spn, type_of_map, file_name):
     map_request = "http://static-maps.yandex.ru/1.x/"
     params = {
         'll': ','.join([str(coord_1), str(coord_2)]),
         'spn': ','.join([str(spn), str(spn)]),
-        'l': 'map'
+        'l': type_of_map
     }
     response = requests.get(map_request, params=params)
 
@@ -17,7 +17,7 @@ def map(coord_1, coord_2, spn):
         print("Http статус:", response.status_code, "(", response.reason, ")")
         sys.exit(1)
 
-    map_file = "map.png"
+    map_file = file_name
     with open(map_file, "wb") as file:
         file.write(response.content)
 
